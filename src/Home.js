5176e29a9755d4389da097fe6cd4facb43e17833
Student <example@example.com>
2020-02-28 09:10:10 -0800

    whoops

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.png';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+      </header>
+    </div>
+  );
+}
+
+export default Home;
