3f5c1e479fa30a4a35efea1e9448284aabfc8b1c
Nate Powers <russellsproutz@gmail.com>
2020-03-05 12:57:08 -0800

    format carousel

--- src/components/Pictures.jsx
+++ src/components/Pictures.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import { Navbar, Nav, NavDropdown, Carousel, Caption,  } from 'react-bootstrap';
 import GoogleFontLoader from 'react-google-font-loader';
 import { Link } from 'react-router-dom';
-import 1 from './smashimages/1.jpg'
-import 2 from './smashimages/2.jpg'
-import 3 from './smashimages/3.jpg'
-import 4 from './smashimages/4.jpg'
-import 5 from './smashimages/5.jpg'
-import 6 from './smashimages/6.jpg'
-import 7 from './smashimages/7.jpg'
-import 8 from './smashimages/8.jpg'
-import 9 from './smashimages/9.jpg'
-import 10 from './smashimages/10.jpg'
-import 11from './smashimages/11.jpg'
-import 1 from './smashimages/1.jpg'
+import s1 from './smashimages/s1.jpg';
+import s2 from './smashimages/s2 .pg';
+import s3 from './smashimages/3.jpg';
+import s4 from './smashimages/s4.jpg';
+import s5 from './smashimages/s5.jpg';
+import s6 from './smashimages/s6.jpg';
+import s7 from './smashimages/s7.jpg';
+import s8 from './smashimages/s8.jpg';
+import s9 from './smashimages/9.jpg';
+import s10 from './smashimages/s10.jpg';
+import s11from './smashimages/s11.jpg';
+
 
 function Pictures (props){
 
@@ -23,8 +23,8 @@ return(
     <Carousel.Item>
       <img
         className="d-block w-100"
-        src="holder.js/800x400?text=First slide&bg=373940"
-        alt="First slide"
+        src={s1}
+        alt="Portal Smashers Image"
       />
       <Carousel.Caption>
         <h3>First slide label</h3>
@@ -34,8 +34,44 @@ return(
     <Carousel.Item>
       <img
         className="d-block w-100"
-        src="holder.js/800x400?text=Second slide&bg=282c34"
-        alt="Third slide"
+        src={s2}
+        alt="Portal Smashers Image"
+      />
+
+      <Carousel.Caption>
+        <h3>Second slide label</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src={s3}
+        alt="Portal Smashers Image"
+      />
+
+      <Carousel.Caption>
+        <h3>Second slide label</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src={s4}
+        alt="Portal Smashers Image"
+      />
+
+      <Carousel.Caption>
+        <h3>Second slide label</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src={s5} 
+        alt="Portal Smashers Image"
       />
 
       <Carousel.Caption>
@@ -47,7 +83,43 @@ return(
       <img
         className="d-block w-100"
         src="holder.js/800x400?text=Third slide&bg=20232a"
-        alt="Third slide"
+        alt="Portal Smashers Image"
+      />
+
+      <Carousel.Caption>
+        <h3>Second slide label</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src="holder.js/800x400?text=Third slide&bg=20232a"
+        alt="Portal Smashers Image"
+      />
+
+      <Carousel.Caption>
+        <h3>Second slide label</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src="holder.js/800x400?text=Third slide&bg=20232a"
+        alt="Portal Smashers Image"
+      />
+
+      <Carousel.Caption>
+        <h3>Second slide label</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src="holder.js/800x400?text=Third slide&bg=20232a"
+        alt="Portal Smashers Image"
       />
 
       <Carousel.Caption>
