d3c5fd907659b074de7b3f7d1eeb3e1ee723c266
Student <example@example.com>
2020-03-06 10:47:52 -0800

    align new buttons

--- src/components/Enter.jsx
+++ src/components/Enter.jsx
@@ -24,7 +24,7 @@ var homeStyles = {
  marginRight: 'auto',
  marginTop: 'auto',
  marginBottom: 'auto',
- width: '50%',
+ width: '75%',
  maxWidth: '150px',
  maxHeight: '20%',
  imageAlign: 'center',
@@ -39,11 +39,38 @@ var massStyles={
   justifyContent: 'center',
   itemAlign: 'center',
   padding: '20%',
+  textAlign: 'center',
+  color: 'yellow',
 }
 
-var objectCenterStyles={
+var centerIsleStyles ={
+display: 'flex',
+}
 
-  
+var onStyles={
+  paddingBottom: '5%',
+  paddingLeft: '10%',
+  marginTop: '5%',
+
+
+}
+var thStyles={
+  paddingBottom: '5%',
+  paddingRight: '10%',
+  marginTop: '5%',
+
+}
+
+var seStyles={
+  paddingTop: '10%',
+  paddingLeft: '10%',
+  marginBottom: '5%',
+}
+
+var niStyles={
+  paddingTop: '10%',
+  paddingRight: '10%',
+  marginBottom: '5%',
 
 }
 
@@ -52,40 +79,40 @@ var objectCenterStyles={
      <div style={massStyles}>
      <Container  >
   <Row >
-    <Col sm={4}>1
-      <div> <img style={homeStyles} src={wlogo} className="App-mlogo" alt="banner"></img></div>
+    <Col md={4} style={onStyles}>
+      <div> <img style={homeStyles} src={wlogo} className="App-mlogo" alt="banner"></img>EVENTS</div>
     </Col>
-    <Col sm={4}>2
-      <div> <img style={homeStyles} src={wlogo} className="App-mlogo" alt="banner"></img></div>
+    <Col md={4}>2
+      <div> </div>
     </Col>
-    <Col sm={4}>3
-      <div> <img style={homeStyles} src={wlogo} className="App-mlogo" alt="banner"></img></div>
+    <Col md={4}  style={thStyles}>
+      <div> <img style={homeStyles} src={wlogo} className="App-mlogo" alt="banner"></img>LINKS...</div>
     </Col>
   </Row>
 
   <Row>
-    <Col sm={4}>4
-      <div> <img style={objectCenterStyles} src={wlogo} className="App-mlogo" alt="banner"></img></div>
+    <Col md={4} >4
+      <div > </div>
     </Col>
-    <Col sm={4}>5
+    <Col lg={6} style={centerIsleStyles}>
       <div>
       <Link to="/"><img src={logo} className="App-logo" alt="logo" /></Link>
       </div>
     </Col>
-    <Col sm={4}>6
-      <div> <img style={homeStyles} src={wlogo} className="App-mlogo" alt="banner"></img></div>
+    <Col md={4}>6
+      <div> </div>
     </Col>
   </Row>
 
   <Row>
-    <Col sm={4}>7
-      <div> <img style={homeStyles} src={wlogo} className="App-mlogo" alt="banner"></img></div>
+    <Col md={4} style={seStyles}>7
+      <div> <img style={homeStyles} src={wlogo} className="App-mlogo" alt="banner"></img>MAKE CONTACT</div>
     </Col>
-    <Col sm={4}>8
-      <div> <img style={homeStyles} src={wlogo} className="App-mlogo" alt="banner"></img></div>
+    <Col md={4}>8
+      <div> </div>
     </Col>
-    <Col sm={4}>9
-      <div> <img style={homeStyles} src={wlogo} className="App-mlogo" alt="banner"></img></div>
+    <Col md={4} style={niStyles}>9
+      <div> <img style={homeStyles} src={wlogo} className="App-mlogo" alt="banner"></img>ABOUT</div>
     </Col>
   </Row>
 </Container>
