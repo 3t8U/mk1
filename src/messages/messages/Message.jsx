bb03a8107826dbb18c8d250fbcb124b0b76e562c
Nate Powers <russellsproutz@gmail.com>
2020-03-10 14:01:50 -0700

    start to vintegrate message boarda

---
+++ src/messages/messages/Message.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import NavBar from './NavBar'
+import Logo from './Logo'
+
+export default function Message({ msgItem }) {
+ //<div>{msgItem.msg}</div>
+
+ return (
+   <div className="message" style={{ paddingBottom: "20px" }}>
+     <b>{msgItem.time}</b>
+     <br />
+     {msgItem.msg}
+   </div>
+ );
+}
